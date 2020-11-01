8960ccdc7988847b18de3802f3ff16c8ba91ca69
Ulrich Schuster <ulrich.schuster@koing.de>
2020-11-01 10:48:58 +0100

    Fix review comments

--- core/serializers/devices.py
+++ core/serializers/devices.py
@@ -39,7 +39,7 @@ class NodeModelSerializer(serializers.HyperlinkedModelSerializer):
 class NodeSerializer(serializers.HyperlinkedModelSerializer):
     related_serializers = {
         "installations": "core.serializers.RoomNodeInstallationSerializer",
-        "timeseries": "SampleListSerializer"
+        "timeseries": "core.serializers.SampleListSerializer"
     }
 
     # A Node is installed in one or more rooms over its lifetime.
