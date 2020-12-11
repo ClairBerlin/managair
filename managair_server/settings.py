f0b03518558913dc2757ebba43be09cd6debf09a
Christian Rotzoll <ch.rotzoll@gmail.com>
2020-12-11 14:01:29 +0100

    Add german translations for the accounts app
    Also set the german as the default language

--- managair_server/settings.py
+++ managair_server/settings.py
@@ -263,14 +263,10 @@ if not DEBUG:
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
-
+LANGUAGE_CODE = "de"
 TIME_ZONE = "UTC"
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
